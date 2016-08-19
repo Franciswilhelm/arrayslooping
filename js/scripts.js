8bba110c92fe50f8f412bfb3264db844bed2d964
Francis Prus <francis.prus@hotmail.com>
2016-08-19 10:32:16 -0700

    add js functions

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,26 @@
+function count(num) {
+  var s = "";
+  for(var i = 1; i <= num; i++) {
+    s += i;
+
+    if (i < (num)) {
+      s += ', ';
+    }
+  }
+  return s;
+}
+
+
+
+
+
+
+
 $(document).ready(function() {
+  $("form#number").submit(function(event) {
+    var numberInput = $("input#number").val();
+    event.preventDefault();
+
+     $("#output").show();
+  });
+});
