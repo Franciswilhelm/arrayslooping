6ec99e05162c9366b0af4f341d7fef8d506490e2
Francis Prus <francis.prus@hotmail.com>
2016-08-29 22:53:06 -0700

    fixed bugs.

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 var pingPong = function(userDigit) {
   var numbers = [];
   for (var i = 1; i <= userDigit; i++) {
@@ -8,51 +8,22 @@ var pingPong = function(userDigit) {
       numbers.push("ping");
     } else if (i % 5 === 0) {
       numbers.push("pong");
-=======
-var numbers = [];
-var result = "";
-function pingPong (userDigit) {
-  for (var i = 1; i <= userDigit; i++) {
-    if (i % 15 === 0) {
-      numbers.append("pingpong");
-    } else if (i % 3 === 0) {
-      numbers.append("ping");
-    } else if (i % 5 === 0) {
-      numbers.append("pong");
->>>>>>> 4b4e8145e5446c14ddbc596b8ad57130f6cdc4ae
     } else {
       numbers.push(i);
-    };
+      }
+    }
+      return numbers;
   };
-<<<<<<< HEAD
-  return numbers;
-};
 
 $(document).ready(function() {
   $("form#counter").submit(function(event) {
     var userDigit = parseInt($("input#number").val());
+    $("#output").empty();
     var resultCount = pingPong(userDigit);
     resultCount.forEach(function(i) {
       $("#output").append("<li>" + i + "</li>");
     });
     $("#show-result").show();
-=======
-  result = parseInt(numbers.join(', '));
-  return result;
-};
-
-
-
-
-
-
-
-$(document).ready(function() {
-  $("form#counter").submit(function(event) {
-    var userDigit = parseInt$("input#userDigit").val();
-
-    $("#output").append(pingPong(userDigit));
->>>>>>> 4b4e8145e5446c14ddbc596b8ad57130f6cdc4ae
     event.preventDefault();
   });
 });
